df38c2eb98914e1ddecf797a3936eacae9cbfc0e
Sudharsh Subbaraman <sudhsubb@cisco.com>
2021-03-12 15:58:44 -0600

    adding the step4.py with the initial write up on how i am going to implement it

---
+++ step4.py
@@ -0,0 +1,6 @@
+"""
+Very flexible Object oriented fizz buzz.
+
+In this my general apporach will be to create seperate rule class.
+extended it for the differet possible rules and create a chain of rules to apply.
+"""
